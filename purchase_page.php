35dc34a80086251e95dd852419be2af6e6e4ca5a
Trevan X Lang <langtrevan@gmail.com>
2015-03-26 08:00:32 -0500

    3/26 purchase page

--- purchase_page.php
+++ purchase_page.php
@@ -38,8 +38,44 @@ else
 	echo "Or click <a href='222account_creationB.php'>here to create an artist account</a>.<br />";
 	echo "you need to be logged in to purchase posters."
 }
-
-
+echo "<TABLE><CAPTION>Your Cart:</CAPTION>";
+$closed_tr = 0; // flag, used to determine if we are at the end of a row when the loop terminates
+$num_images = mysql_num_rows($image_info_table)
+if ($image_info_table)
+	{
+	$poster_count = 0;
+	for ($count = 0; $count < $num_images; $count++)
+		if ($count == $cart)
+		{
+			// The following few lines store information from specific cells in the data about an image
+			$image_row = mysql_fetch_row($image_info_table); // Advances a row each time it is called
+			$image_name = $image_row[1];
+			$thumb_name = $image_row[2];
+			$image_path = pathinfo($image_name);
+			$id_name = $image_path['filename'];
+			$div_id = $id_name . "popin";
+			$poster_count++
+			// Remember the mod operator, this one gives us the remainder when $count is divided by 6
+			if ($count % 6 == 0)
+			{
+				echo "<TR>";
+				$closed_tr = 0;
+			}
+			$domain = $_SERVER['SERVER_NAME'];
+			echo "<TD><img id='$id_name' src='http://$domain/~$dbname/$artistID/$thumb_name' onmouseover=" . '"' . "
+						showDetailedView('$div_id', '$firstName', '$dbname')" . '" />';
+			echo "<div id = '$div_id'></div>Poster Cost: $10</TD>";
+			if ($count % 6 == 5)
+			{
+				echo "</TR>";
+				$closed_tr = 1;
+			}
+		}
+	}
+	if ($closed_tr == 0) echo "</TR>"; // Appends a close tag for the TR element if the loop did not terminate at a row end.
+	echo "</TABLE>";
+}
+echo "<TH align='center'>Total cost: $" . $poster_count . "0";
+echo "<button type='button' onclick='$cart = []'>Clear cart</button>"
 echo "<TH align='right'>Click <a href='221indexB.html'>here to return to the gallery</a>.</TH><br />";
-
 ?>
